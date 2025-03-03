44c18a591cc06027b97c9f82cd25397822bc6c77
Tovah Marx <tovah.marx@gmail.com>
2025-03-02 21:28:41 -0700

    Initial Commit

---
+++ Fitness App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness App
+//
+//  Created by Tovah Marx on 3/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Tovah Marx on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
